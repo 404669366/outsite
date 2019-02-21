d73736160968343a0207512f1c97e94e6edcdddd
DESKTOP-56SB0F0\miku <404669366@qq.com>
2019-02-21 14:08:52 +0800

    no message

--- mobile/views/login/login/login.php
+++ mobile/views/login/login/login.php
@@ -14,10 +14,10 @@
         <input type="hidden" name="_csrf" value="<?= Yii::$app->request->csrfToken ?>">
         <div class="tit">博瑞恩贝</div>
         <div class="text">
-            <input class="tel" name="tel" type="text" placeholder="账号">
+            <input class="tel" name="loginTel" type="text" placeholder="账号">
         </div>
         <div class="text">
-            <input class="code" name="code" type="text" placeholder="验证码">
+            <input class="code" name="loginTelCode" type="text" placeholder="验证码">
             <div class="click">发送</div>
         </div>
         <button class="text button" type="submit">登 录</button>

b38431590599890edbe8c73c4b8cfe93924c257c
DESKTOP-56SB0F0\miku <404669366@qq.com>
2019-02-22 14:55:10 +0800

    no message

--- mobile/views/user/active/list.php
+++ mobile/views/user/active/list.php
@@ -1,1 +1,12 @@
-活动列表
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>博瑞恩贝 - 活动列表</title>
+    <script src="/resources/js/common.js" type="text/javascript" charset="utf-8"></script>
+    <?php \vendor\project\helpers\Msg::run() ?>
+</head>
+<body>
+
+</body>
+</html>

ffe618db2622b23d04394ab30f86d8c2b0b32b53
DESKTOP-56SB0F0\miku <404669366@qq.com>
2019-02-22 16:26:54 +0800

    no message

--- mobile/views/basis/basis/error.php
+++ mobile/views/basis/basis/error.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>404</title>
     <script src="/resources/js/common.js" type="text/javascript" charset="utf-8"></script>
+    <link href="/resources/css/error.css" rel="stylesheet">
     <?php \vendor\project\helpers\Msg::run() ?>
 </head>
 <body>
@@ -12,7 +13,7 @@
         404
         <div class="cont">页面不见啦!</div>
     </div>
-    <div class="notMain" id="notMain">10秒后页面将跳转到上一页...</div>
+    <div class="notMain" id="notMain">10秒后将跳回上一页...</div>
     <div class="notBtn">
         <a href="javascript:history.back(-1)">立即跳转</a>&emsp;&emsp;
         <a href="/user/user/center.html">返回个人中心</a>
@@ -23,7 +24,7 @@
     var num = 10;
     var id = setInterval(function () {
         num--;
-        document.getElementById('notMain').innerText = num + "秒后页面将跳转到上一页...";
+        document.getElementById('notMain').innerText = num + "秒后将跳回上一页...";
         if (num === 0) {
             clearInterval(id);
             history.back(-1);

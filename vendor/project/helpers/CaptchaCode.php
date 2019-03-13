31016b3a5a12e07b2bfc007e98273a60f38b63ca
DESKTOP-DSUIKPF\lt <244652460@qq.com>
2019-03-13 14:38:02 +0800

    no message

--- vendor/project/helpers/CaptchaCode.php
+++ vendor/project/helpers/CaptchaCode.php
@@ -135,7 +135,6 @@ class CaptchaCode
     public static function validate($code = '', $model = '')
     {
         if ($code && $model) {
-            @session_start();
             $old = \Yii::$app->session->get($model);
             if ($old == strtolower($code)) {
                 return true;

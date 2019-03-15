f86a2cd3f9153a5199f1ab24a07e389f9b20611e
DESKTOP-DSUIKPF\lt <244652460@qq.com>
2019-03-15 19:26:07 +0800

    no message

--- vendor/project/database/User.php
+++ vendor/project/database/User.php
@@ -127,6 +127,7 @@ class User extends \yii\db\ActiveRecord implements IdentityInterface
             $data = $objPHPExcel->getSheet(0)->toArray();
             $data = Helper::assocUnique($data, 0);
             $keys = ['家长电话', '家长姓名', '学生姓名', '学生性别', '学生年龄', '班级'];
+            var_dump(array_intersect($data[0], $keys));
             if (array_intersect($data[0], $keys) == $keys) {
                 $sex = [
                     '男' => 0,

d367c3ad08ec5d880e83d14663dcced90b2e5dbb
DESKTOP-56SB0F0\miku <404669366@qq.com>
2019-03-21 12:35:50 +0800

    no message

--- vendor/project/helpers/Forbidden.php
+++ vendor/project/helpers/Forbidden.php
@@ -16,7 +16,7 @@ class Forbidden
     public static function isForbidden()
     {
         if (redis::app()->get('AppForbidden') === '禁用') {
-            echo '<div style="position: fixed;display: table;left: 0;top: 0;width: 100%;height: 100%"><span style="display:table-cell;vertical-align: middle;text-align: center;font-size: 20px">The server is forbidden</span></div>';
+            echo '<div style="position: fixed;display: table;left: 0;top: 0;width: 100%;height: 100%"><span style="display:table-cell;vertical-align: middle;text-align: center;font-size: 20px">Access to this server is forbidden</span></div>';
             return true;
         }
         redis::app()->set('AppForbidden', '启用');
